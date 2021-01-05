83c4076d14434cc6d3faa417fd1242c277de21b0
marcopeg <marco.pegoraro@gmail.com>
2021-01-05 07:19:47 +0000

    Wip on fast setup

--- src/setup.sh
+++ src/setup.sh
@@ -2,17 +2,21 @@
 
 CODE_SERVER_SETUP_SRC_CWD="$(dirname "$0")"
 
-# Setup:
+# Install basic software:
 source "${CODE_SERVER_SETUP_SRC_CWD}/setup-init.sh"
 source "${CODE_SERVER_SETUP_SRC_CWD}/setup-apt-get.sh"
 source "${CODE_SERVER_SETUP_SRC_CWD}/setup-env.sh"
 source "${CODE_SERVER_SETUP_SRC_CWD}/setup-bashrc.sh"
 source "${CODE_SERVER_SETUP_SRC_CWD}/setup-software-code-server.sh"
 source "${CODE_SERVER_SETUP_SRC_CWD}/setup-software-docker.sh"
-source "${CODE_SERVER_SETUP_SRC_CWD}/setup-software-ctop.sh"
-source "${CODE_SERVER_SETUP_SRC_CWD}/setup-software-nvm.sh"
-source "${CODE_SERVER_SETUP_SRC_CWD}/setup-software-dotnet.sh"
-source "${CODE_SERVER_SETUP_SRC_CWD}/setup-software-aws-cli.sh"
+
+# Install additional software:
+# source "${CODE_SERVER_SETUP_SRC_CWD}/setup-software-ctop.sh"
+# source "${CODE_SERVER_SETUP_SRC_CWD}/setup-software-nvm.sh"
+# source "${CODE_SERVER_SETUP_SRC_CWD}/setup-software-dotnet.sh"
+# source "${CODE_SERVER_SETUP_SRC_CWD}/setup-software-aws-cli.sh"
+
+# Boot the machine:
 source "${CODE_SERVER_SETUP_SRC_CWD}/setup-final.sh"
 
 # First boot:
