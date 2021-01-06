b4a276b374932392805a7ab29edb37339254da0a
marcopeg <marco.pegoraro@gmail.com>
2021-01-06 08:20:50 +0000

    Install all without starting

--- src/setup.sh
+++ src/setup.sh
@@ -12,12 +12,12 @@ source "${CODE_SERVER_SETUP_SRC_CWD}/setup-software-docker.sh"
 
 # Install additional software:
 source "${CODE_SERVER_SETUP_SRC_CWD}/setup-software-ctop.sh"
-# source "${CODE_SERVER_SETUP_SRC_CWD}/setup-software-nvm.sh"
-# source "${CODE_SERVER_SETUP_SRC_CWD}/setup-software-dotnet.sh"
-# source "${CODE_SERVER_SETUP_SRC_CWD}/setup-software-aws-cli.sh"
+source "${CODE_SERVER_SETUP_SRC_CWD}/setup-software-nvm.sh"
+source "${CODE_SERVER_SETUP_SRC_CWD}/setup-software-dotnet.sh"
+source "${CODE_SERVER_SETUP_SRC_CWD}/setup-software-aws-cli.sh"
 
 # Boot the machine:
 source "${CODE_SERVER_SETUP_SRC_CWD}/setup-final.sh"
 
 # First boot:
-source "${CODE_SERVER_SETUP_SRC_CWD}/ec2-ubuntu-boot.sh"
+# source "${CODE_SERVER_SETUP_SRC_CWD}/ec2-ubuntu-boot.sh"
